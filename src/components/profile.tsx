f730fcaac687e456b1e863340f4d38b4daeb6311
wish-in <wish.in@icloud.com>
2025-04-16 22:53:17 +0900

    Modify profile description

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,10 +1,10 @@
 export default function Profile() {
   return (
     <section className="pb-10">
-      <h2 className="text-lg font-medium">Hi, I'm wish.in.</h2>
+      <h2 className="text-lg font-medium">Hi, I'm wish.in</h2>
       <p className="mt-3">
-        I'm a junior back-end developer based in South Korea. I'm interested in
-        Java, Spring Framework, and Hadoop Ecosystem.
+        쇼핑 서비스를 만드는 주니어 백엔드 개발자입니다. 코틀린, 스프링
+        프레임워크, 하둡 생태계에 관심을 두고 있습니다.
       </p>
     </section>
   );
