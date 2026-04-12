af8005c6f1d7260a90bcb1300eadd008f7e64c67
wzyns <wish.in@icloud.com>
2026-04-12 23:40:50 +0900

    Enable static site generation

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  "output": "export" // static site generation
 };
 
 export default nextConfig;
